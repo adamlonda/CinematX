47e50f928953ee0e527a25241cad1dc5133e7d79
Unknown <adamlonda@gmail.com>
2019-01-23 19:25:17 +0100

    Empty project

---
+++ CinematX/CinematX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CinematX
+//
+//  Created by Adam Londa on 23/01/2019.
+//  Copyright © 2019 Adam Londa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
