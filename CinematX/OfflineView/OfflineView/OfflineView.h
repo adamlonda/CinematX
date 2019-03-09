7f71c1679b588cd481e8bf126225bc72d4553b44
Adam Londa <adamlonda@gmail.com>
2019-03-09 11:24:21 +0100

    OfflineView blank framework

---
+++ CinematX/OfflineView/OfflineView/OfflineView.h
@@ -0,0 +1,19 @@
+//
+//  OfflineView.h
+//  OfflineView
+//
+//  Created by Adam Londa on 09/03/2019.
+//  Copyright © 2019 Adam Londa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OfflineView.
+FOUNDATION_EXPORT double OfflineViewVersionNumber;
+
+//! Project version string for OfflineView.
+FOUNDATION_EXPORT const unsigned char OfflineViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OfflineView/PublicHeader.h>
+
+
